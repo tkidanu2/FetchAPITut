f442b3bf4646ddf887b5e2f1f011df622bd7f020
Tawer Aradom Kidanu <tkidanu@luc.edu>
2025-05-10 01:56:55 -0500

    add error handling and response processing

---
+++ index.js
@@ -0,0 +1,10 @@
+const band = {
+    vocals: "Robert Plant",
+    guitar: "Jimmy Page",
+    bass: "John Paul Jones",
+    drums: "John Bonham"
+};
+
+for (let job in band){
+    console.log(job);
+}
